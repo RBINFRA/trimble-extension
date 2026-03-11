fd9aefb1bd93be585478b9dc6c6df59d695aab15
RBINFRA <r.benoit@infra-concept.com>
2026-03-11 15:59:27 +0100

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Extension Trimble chargée");
+import * as WorkspaceAPI from "https://unpkg.com/trimble-connect-workspace-api";
+
+async function connectAPI() {
+
+  const API = await WorkspaceAPI.connect(window.parent, (event, args) => {
+
+    console.log("Event:", event, args);
+
+    if (event === "viewer.selection.changed") {
+      document.getElementById("properties").innerHTML =
+        "Objet sélectionné dans la maquette";
+    }
+
+  });
+
+}
+
+connectAPI();
