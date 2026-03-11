0cae25294eb55f28cbfa57423b361919226d0647
RBINFRA <r.benoit@infra-concept.com>
2026-03-11 16:37:07 +0100

    app.js

--- app.js
+++ app.js
@@ -46,21 +46,4 @@ function afficherProprietes(props) {
 }
 
 // Lancement de la connexion
-connectAPI();import * as WorkspaceAPI from "https://unpkg.com/trimble-connect-workspace-api";
-
-async function connectAPI() {
-
-  const API = await WorkspaceAPI.connect(window.parent, (event, args) => {
-
-    console.log("Event:", event, args);
-
-    if (event === "viewer.selection.changed") {
-      document.getElementById("properties").innerHTML =
-        "Objet sélectionné dans la maquette";
-    }
-
-  });
-
-}
-
 connectAPI();
